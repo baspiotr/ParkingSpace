74a42175822b65f93a61b91596d4454530d0291b
Piotr Basinski <piotrbasinski@Piotrs-MacBook-Pro.local>
2018-02-27 12:04:24 +0100

    Initial Commit

---
+++ parkingspaces/src/main/java/com/baspiotr/parkingspaces/ParkingspacesApplication.java
@@ -0,0 +1,12 @@
+package com.baspiotr.parkingspaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingspacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingspacesApplication.class, args);
+	}
+}
